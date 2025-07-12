9edf63ffd5d45b9240cd174aae2de830957f3ae7
1034989 <1034989@student.sae.edu.au>
2025-07-12 23:24:14 +1000

    AI armed
    The AI can now attack the player as intended, this is final ver and should just need to be laid out

--- Assets/AI Scripting/Scripts/RangedAI.cs
+++ Assets/AI Scripting/Scripts/RangedAI.cs
@@ -6,7 +6,8 @@ namespace EnemyAI
 {
     public class RangedAI : AIMovement
     {
-
+        public float raycastDistance = 10f; // max shooting distance
+        public LayerMask hitLayer; // layer which can be hit
         // Start is called before the first frame update
         void Start()
         {
@@ -22,6 +23,7 @@ namespace EnemyAI
             moveEnemy();
             moveAngle(); //to change the enemy character's rotation to face the player character
             Hold();
+            raycastShoot();
         }
         void Hold()
         {
@@ -34,6 +36,23 @@ namespace EnemyAI
             {
                 // if player is further than 10 keep moving
                 speed = 3f; 
+            }
+        }
+        void raycastShoot()
+        {
+            Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.red);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance, hitLayer))
+            {
+                if (hit.transform.CompareTag("Player")) // Check if the hit object is tagged as "Player"
+                {
+                    Debug.Log("Enemy hit you");
+                    //moves the Player randomly
+                    hit.collider.transform.position = new Vector3(UnityEngine.Random.Range(-150, 150), 2, UnityEngine.Random.Range(-150, 150));
+                }
+
             }
         }
     }
