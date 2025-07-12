ad45a680574ace9f16b57a4a4c8e56c0c32f7a57
1034989 <1034989@student.sae.edu.au>
2025-07-12 22:01:04 +1000

    GunScript Added
    Added the first half of the gun weapons system

---
+++ Assets/GunFolder/GunScripting/weaponList.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponList", menuName = "WeaponList", order = 0)]
+public class Weapon : ScriptableObject
+{
+    public string weaponName;
+    public int ammoCount;
+    public int clipSize;
+
+    private void OnEnable()
+    {
+        // ammo is full when the game starts
+        ammoCount = clipSize;
+    }
+    public void ShootWeapon()
+    {
+        ammoCount--;
+        Debug.Log("Shooting " + weaponName + ". Ammo left: " + ammoCount + "/" + clipSize);
+    }
+}
